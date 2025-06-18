b23ddb5f1cdad1a8ecd384577071740269e523d0
Is Santi <its.santinoriveros@gmail.com>
2025-06-18 19:55:32 -0300

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -3,7 +3,7 @@ import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
 let timestamp = speed()
-let sentMsg = await conn.reply(m.chat, '❀ Calculando ping...', m)
+let sentMsg = await conn.reply(m.chat, '🔆Calculando ping...', m)
 let latency = speed() - timestamp
 exec(`neofetch --stdout`, (error, stdout, stderr) => {
 let child = stdout.toString("utf-8");
