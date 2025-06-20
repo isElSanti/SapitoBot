b1eb5a1d1b8a15dc0ec8322371d185fcf27cc4fe
Is Santi <its.santinoriveros@gmail.com>
2025-06-19 21:14:37 -0300

    Update owner-setstatus.js

--- plugins/owner-setstatus.js
+++ plugins/owner-setstatus.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-   if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingresa la nueva biografia que deseas ponerme.`, m)
+   if (!text) return conn.reply(m.chat, `${emoji} ingresa la nueva biografia que deseas ponerme.`, m)
      try {
                 await conn.updateProfileStatus(text).catch(_ => _)
                 conn.reply(m.chat, `${emoji} Info Cambiada Con Exito...`, m)
